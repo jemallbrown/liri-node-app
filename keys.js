a4c35ecc2dd20ebb7bb389c173c5a926f778721f
JayBee <jemallbrown@gmail.com>
2018-11-15 20:40:58 -0500

    edits and added inquirer interface to homework

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: 'f86b097a7dc94d03a1f599641cbc2d1e',
-  secret: '77d0bcf62efa4d5b89ef8f3509dca0c6'
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
 
--- liri.js
+++ liri.js
@@ -1,129 +1,91 @@
 require("dotenv").config();
- 
-// require("./keys").config();
-var request = require("request");
-var spotify = require('node-spotify-api');
-var bandsintown = require('bandsintown');
-var moment = require('moment');
+var dataKeys = require("./keys.js")
 var fs = require("fs");
+var Spotify = require('node-spotify-api');
+var request = require("request");
+var moment = require('moment');
 var found = false
-
-
-var spotify = new spotify({
-    id: 'f86b097a7dc94d03a1f599641cbc2d1e',
-    secret: '77d0bcf62efa4d5b89ef8f3509dca0c6'
-  });
-
-var command = process.argv[2];
+var dataArray = [];
 var data = process.argv.slice(3).join("-");
-
+var inquirer = require('inquirer');
+var cmd = ""
+var choice = ""
+var loopcount = 1
 console.log(data)
 
-switch (command) {
-    
-    case 'concert-this':
-    console.log("you want concert info on",data)
-       
-        request("https://rest.bandsintown.com/artists/"+data+"/events?app_id=codingbootcamp", function(error, response, body) {
+// Concert this function
 
-            // If the request is successful (i.e. if the response status code is 200)
-            if (!error && response.statusCode === 200) {
-        
-                parsedData = (JSON.parse(body));
-                if (parsedData.length === 0){
-                    console.log("Nothing found");
-                }
-
-                else {
-                    for (let i = 0; i < parsedData.length; i++) {
-                        console.log('EVENT NUMBER ',(i+1))
-                        console.log("Name of venue - ",parsedData[i].venue.name);
-                        console.log("Venue location - ",parsedData[i].venue.city);
-                        var date = (parsedData[i].datetime);
-                        moment(date, "MM-DD-YYYY");
-                        console.log("Date of the Event - ",date);
-                        console.log(" ")
-                        found = true
-                    }
-                }
-
-                if (found === true){
-                    dataArray = [command + " " + data]
-                    logToFile();
-                    
-                }
-
-                }
-            });
-
-        
-    break;
-    
-    case 'spotify-this-song':
-    // console.log("you want spotify info on",data)
-    if(data === ""){
-        // console.log("nothing to look for")
-        spotify
-        .request('https://api.spotify.com/v1/search?query=the-sign&type=track&limit=5')
-        .then(function(response) {
-                // console.log(response)
-                console.log("The artist name is - " + response.tracks.items[0].artists[0].name);
-                console.log("The songs name is - " + response.tracks.items[0].name); 
-                console.log("A preview link of the song from Spotify - " + response.tracks.items[0].preview_url);
-                console.log("The album that the song is from - " + response.tracks.items[0].album.name);
-                console.log(" ")
-                return;
-         
-        })
-
-        }
-        else {
-    spotify
-    .request('https://api.spotify.com/v1/search?query=' + data + '&type=track&limit=5')
-    .then(function(response) {
-    
-            console.log("The artist name is - " + response.tracks.items[0].artists[0].name);
-            console.log("The songs name is - " + response.tracks.items[0].name); 
-            console.log("A preview link of the song from Spotify - " + response.tracks.items[0].preview_url);
-            console.log("The album that the song is from - " + response.tracks.items[0].album.name);
-            console.log(" ")
-            found = true;
-            
-        
-        if (found === true){
-            dataArray = [command + " " + data]
-            logToFile();
-            
-        }
-    })
-    .catch(function(err) {
-        console.error('Error occurred: ' + err); 
-    });
-        }
-            
-    break;
-
-    case 'movie-this':
-    console.log("Details for the movie",data)
-
-    if (data === ''){
-        request("http://www.omdbapi.com/?t=mr-nobody&y=&plot=short&tomatoes=true&apikey=trilogy", function(error, response, body) {
-            console.log("Title of the movie - "+JSON.parse(body).Title);
-            console.log("Year the movie came out - "+JSON.parse(body).Year);
-            console.log("IMDB Rating of the movie - " + JSON.parse(body).imdbRating);
-            console.log("Rotten Tomatoes Rating of the movie - " + JSON.parse(body).tomatoRating);
-            console.log("Country where the movie was produced - " + JSON.parse(body).Country);
-            console.log("Language of the movie - " + JSON.parse(body).Language);
-            console.log("Plot of the movie - " + JSON.parse(body).Plot);
-            console.log("Actors in the movie - " + JSON.parse(body).Actors);
-        });
-        break;
-    }
-    request("http://www.omdbapi.com/?t="+data+"&y=&plot=short&tomatoes=true&apikey=trilogy", function(error, response, body) {
+var concertThis = function(){
+request("https://rest.bandsintown.com/artists/"+data+"/events?app_id=codingbootcamp", function(error, response, body) {
 
     // If the request is successful (i.e. if the response status code is 200)
     if (!error && response.statusCode === 200) {
 
+        parsedData = (JSON.parse(body));
+        if (parsedData.length === 0){
+            console.log("Nothing found");
+        }
+
+        else {
+            for (let i = 0; i < parsedData.length; i++) {
+                console.log('EVENT NUMBER ',(i+1))
+                console.log("Name of venue - ",parsedData[i].venue.name);
+                console.log("Venue location - ",parsedData[i].venue.city);
+                var date = (parsedData[i].datetime);
+                moment(date, "MM-DD-YYYY");
+                console.log("Date of the Event - ",date);
+                console.log(" ")
+                found = true
+            }
+        }
+
+        if (found === true){
+            dataArray = [cmd + " " + data]
+            logToFile();
+            
+        }
+
+        }
+    });
+};
+
+
+// Spotify function
+var spotifyThisSong = function() {
+    // console.log("you want spotify info on "+ command+","+data)
+
+    let spotify = new Spotify(dataKeys.spotify);
+    // If there is no song name, set the song to Backstreet boys "I want it that way"
+    if (!data) {
+        data = "I want it that way";
+    }
+    spotify.search({ type: 'track', query: data }, function(err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        } else {
+
+                console.log("The artist name is - " + data.tracks.items[0].artists[0].name);
+                console.log("The songs name is - " + data.tracks.items[0].name); 
+                console.log("A preview link of the song from Spotify - " + data.tracks.items[0].preview_url);
+                console.log("The album that the song is from - " + data.tracks.items[0].album.name);
+                console.log(" ")
+                found = true;
+            }
+        });
+   
+
+        // store the searched for command and song in an array that will be dumped to the log.txt file
+        dataArray = [cmd + " " + data]
+        console.log("data array is "+dataArray)
+        logToFile();
+
+} 
+
+// Movie this function
+var movieThis = function(){
+if (data === ''){
+    request("http://www.omdbapi.com/?t=mr-nobody&y=&plot=short&tomatoes=true&apikey=trilogy", function(error, response, body) {
         console.log("Title of the movie - "+JSON.parse(body).Title);
         console.log("Year the movie came out - "+JSON.parse(body).Year);
         console.log("IMDB Rating of the movie - " + JSON.parse(body).imdbRating);
@@ -132,76 +94,149 @@ switch (command) {
         console.log("Language of the movie - " + JSON.parse(body).Language);
         console.log("Plot of the movie - " + JSON.parse(body).Plot);
         console.log("Actors in the movie - " + JSON.parse(body).Actors);
-        found = true;
-        }
     });
-    if (found === true){
-        dataArray = [command + " " + data]
-        logToFile();
-        
-    }
-
-                
-    break;
-
-    case 'do-what-it-says':
-    console.log("do what is says is chosen");
-
-    // i figured out how to do this but it will require some code rewriting.  I should have made each switch
-    // statment a seperate method and then I could have just call the switch method from the begining
-    // or if the switch was itself a method, I could have then called within this seperate case
-    // Unfortunately I don't have time to correct this now and submit homework on time so I will
-    // correct at a later date.
-
-    fs.readFile("./log.txt", "utf8", function(err, data) {
-        if (err) {
-          return console.log(err);
-        }
     
-        // Break down all the information inside log.txt
-        dataArray = data.split(", ");
-        var chosenArray = dataArray[0].split(" ");
-        // console.log(chosenArray[0])
-        // console.log(chosenArray[1])
-        command = (chosenArray[0])
-        data = (chosenArray[1])
-        console.log(command);
-        console.log(data)
-        command();
+}
+request("http://www.omdbapi.com/?t="+data+"&y=&plot=short&tomatoes=true&apikey=trilogy", function(error, response, body) {
 
-        
-        
+// If the request is successful (i.e. if the response status code is 200)
+if (!error && response.statusCode === 200) {
+
+    console.log("Title of the movie - "+JSON.parse(body).Title);
+    console.log("Year the movie came out - "+JSON.parse(body).Year);
+    console.log("IMDB Rating of the movie - " + JSON.parse(body).imdbRating);
+    console.log("Rotten Tomatoes Rating of the movie - " + JSON.parse(body).tomatoRating);
+    console.log("Country where the movie was produced - " + JSON.parse(body).Country);
+    console.log("Language of the movie - " + JSON.parse(body).Language);
+    console.log("Plot of the movie - " + JSON.parse(body).Plot);
+    console.log("Actors in the movie - " + JSON.parse(body).Actors);
+    found = true;
     }
-        );
-        
-                    
-    break;
+});
+if (found === true){
+    dataArray = [cmd + " " + data]
+    logToFile();
     
-    
-    default:
-
-        break;
-
-    }
-
-var logToFile = function(){
-
-fs.appendFile("log.txt", (dataArray+", "), function(err) {
-
-    // If an error was experienced we will log it.
-    if (err) {
-      console.log(err);
-    }
-  
-    // If no error is experienced, we'll log the phrase "Content Added" to our node console.
-    else {
-      console.log("Content Added to log.txt!");
-    }
-  
-  });
+}
 };
 
 
+// This is the function where we store searched for detail into the log.txt file
+var logToFile = function(){
 
- // We will read the existing bank file
- 
+    fs.appendFile("log.txt", (dataArray+", "), function(err) {
+    
+        // If an error was experienced we will log it to the node console.
+        if (err) {
+          console.log(err);
+        }
+      
+        // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+        else {
+          console.log("Detail successfully added to the log.txt file");
+        }
+      
+      });
+    };
+
+
+// This is the do-what-it-says function wihch will read from the random.txt file and "Do what it says"
+var doWhatItSays = function(){
+    fs.readFile("./random.txt", "utf8", function(err, data) {
+                if (err) {
+                  return console.log(err);
+                }
+            
+                // Break down the information inside log.txt and then dump it to the runTheSearch function
+                dataArray = data.split(",");
+                // console.log(dataArray[0])
+                // console.log(dataArray[1])
+                cmd = (dataArray[0])
+                data = (dataArray[1])
+                runTheSearch(cmd);
+                
+            }
+                );
+    
+}
+
+var askLiri = function(){
+    // Create a "Prompt" to begin the liri.app application.
+    inquirer
+      .prompt([
+        // User can choose from a list of functions as outlined in the homework assignment.
+        {
+          type: "list",
+          message: "Which search type would you like liri to use?",
+          choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"],
+          name: "searchType"
+        },
+        //  Here we create a text prompt to determine what will be searched for
+        {
+        type: "input",
+        message: "What do you want to search for?",
+        name: "userData"
+        },
+        // Here we ask the user to confirm they want to do the search as is.
+        {
+          type: "confirm",
+          message: "Are you sure:",
+          name: "confirm",
+          default: true
+        }
+      ])
+      .then(function(inquirerResponse) {
+        // If the inquirerResponse confirms, we display the details of the search and kick of the runWithIt function
+        if (inquirerResponse.confirm) {
+            cmd = inquirerResponse.searchType
+            data = inquirerResponse.userData
+          console.log("You've Chosen to search type " + cmd + "\n");
+          console.log("You've Chosen to search for " + data + "\n");
+          runTheSearch(cmd);
+          
+        }
+        else {
+            if (loopcount < 3){
+                loopcount +=1
+                console.log("\nLet's try again then.\n");
+                askLiri();
+            };
+            console.log("Too many failed attempt! Try again later.")
+            
+        }
+      });
+    
+    
+    };
+    
+    var runTheSearch =function(cmd){
+        console.log(cmd)
+        switch (cmd) {
+            case "concert-this":
+            console.log("Calling the concert-this function to search for "+data)
+            concertThis();
+                
+                break;
+            case 'spotify-this-song':
+            console.log("you want spotify info on the song "+data)
+            spotifyThisSong();    
+            
+            break;
+
+            case 'movie-this':
+            console.log("Details for the movie ",data)
+            movieThis();
+        
+            break;
+
+            case "do-what-it-says":
+            doWhatItSays();
+
+            break;
+        
+            default:
+            
+            break;
+        }
+    };
+    askLiri();
